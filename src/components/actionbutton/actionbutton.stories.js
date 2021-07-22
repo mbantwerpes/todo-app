3af438da7678cff645d7f7cc0a7b583d5aca8508
mbantwerpes <marcel.blesing@antwerpes.com>
2021-07-22 10:48:08 +0200

    Add actionbutton to storybook

---
+++ src/components/actionbutton/actionbutton.stories.js
@@ -0,0 +1,9 @@
+import "./actionbutton.css";
+import actionbutton from "./actionbutton.html";
+
+export default {
+  title: "Components/Actionbutton",
+  parameters: { layout: "fullscreen" },
+};
+
+export const headers = () => actionbutton;
