1d9463d1b7660569e376c00bd0856363a09487eb
mbantwerpes <marcel.blesing@antwerpes.com>
2021-07-23 14:00:06 +0200

    Add taskItem component

---
+++ src/components/taskItem/taskItem.stories.js
@@ -0,0 +1,9 @@
+import "./taskItem.css";
+import taskItem from "./taskItem.html";
+
+export default {
+  title: "Components/TaskItem",
+  parameters: { layout: "centered" },
+};
+
+export const taskItemExp = () => taskItem;
