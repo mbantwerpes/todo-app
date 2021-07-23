b69fb5e99406fb65d8f6a9dfa3b369df8c9e9d38
mbantwerpes <marcel.blesing@antwerpes.com>
2021-07-23 16:13:22 +0200

    Move page to new pages folder

--- src/components/pages/addTask/addTask.stories.js
+++
@@ -1,9 +0,0 @@
-import "./addTask.css";
-import addTask from "./addTask.html";
-
-export default {
-  title: "Components/AddTask",
-  parameters: { layout: "fullscreen" },
-};
-
-export const regular = () => addTask;
