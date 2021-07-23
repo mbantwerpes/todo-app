1c2384dd3d6099be51730f1fdf11c5edf07963d1
mbantwerpes <marcel.blesing@antwerpes.com>
2021-07-23 14:01:38 +0200

    Reanme taskItem to taskList

--- src/components/taskItem/taskItem.stories.js
+++
@@ -1,9 +0,0 @@
-import "./taskItem.css";
-import taskItem from "./taskItem.html";
-
-export default {
-  title: "Components/TaskItem",
-  parameters: { layout: "centered" },
-};
-
-export const taskItemExp = () => taskItem;
