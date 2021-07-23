12a5641889e806d830ff0c89aa822381235cf7f4
mbantwerpes <marcel.blesing@antwerpes.com>
2021-07-23 16:04:59 +0200

    Create folder structure for taskListPage

---
+++ src/pages/taskListPage/taskListPage.stories.js
@@ -0,0 +1,9 @@
+import "./taskListPage.css";
+import taskListPage from "./taskListPage.html";
+
+export default {
+  title: "Components/TaskListPage",
+  parameters: { layout: "fullscreen" },
+};
+
+export const regular = () => taskListPage;
